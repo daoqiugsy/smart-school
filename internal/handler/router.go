b5c1e767a18b1dd793a3db536280d780c07d2351
gongsiyu <1242435445@qq.com>
2026-04-16 23:26:23 +0800

    test

--- internal/handler/router.go
+++ internal/handler/router.go
@@ -19,7 +19,7 @@ func RegisterRoutes(r *gin.Engine, authHandler *AuthHandler, scheduleHandler *Sc
 		}
 	}
 
-	// 需要认证的路由组
+	// 需要
 	protected := r.Group("/api")
 	protected.Use(middleware.JWTAuth())
 	{
